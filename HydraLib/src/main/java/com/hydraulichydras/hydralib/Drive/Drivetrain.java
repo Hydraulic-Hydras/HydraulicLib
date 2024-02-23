f64ccb6c81a14810d2d847d4ffc839c7e486d888
Tea <mtauha9@gmail.com>
2024-02-22 22:26:47 -0500

    v1.1.1

--- HydraLib/src/main/java/com/hydraulichydras/hydralib/Drive/Drivetrain.java
+++ HydraLib/src/main/java/com/hydraulichydras/hydralib/Drive/Drivetrain.java
@@ -2,6 +2,13 @@ package com.hydraulichydras.hydralib.Drive;
 
 import com.hydraulichydras.hydralib.Geometry.Pose;
 
+/**
+ * This interface outlines a fundamental functionality related to controlling a drivetrain system
+ */
 public interface Drivetrain {
+    /**
+     * This method specifies a way to set the pose for your drivetrain system
+     * @param pose
+     */
     void set(Pose pose);
 }
--- HydraLib/src/main/java/com/hydraulichydras/hydralib/Util/Localizer.java
+++ HydraLib/src/main/java/com/hydraulichydras/hydralib/Util/Localizer.java
@@ -2,8 +2,23 @@ package com.hydraulichydras.hydralib.Util;
 
 import com.hydraulichydras.hydralib.Geometry.Pose;
 
+/**
+ * This interface, outlines the functionality required for an object responsible for localization within a system.
+ */
 public interface Localizer {
+    /**
+     * This method indicates that the localizer performs periodic actions or updates.
+     */
     void periodic();
+
+    /**
+     * Returns the current pose
+     */
     Pose getPos();
+
+    /**
+     * Sets the pose
+     * @param pose
+     */
     void setPos(Pose pose);
 }
